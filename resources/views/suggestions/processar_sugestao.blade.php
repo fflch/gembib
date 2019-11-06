a75992a1c855b19c8a5f725391366fe5040539e4
Gabriela Dos Reis Silva <gabrielareisg@hotmail.com>
2019-11-06 16:25:59 -0300

    Mudanças processar sugestão

--- resources/views/suggestions/processar_sugestao.blade.php
+++ resources/views/suggestions/processar_sugestao.blade.php
@@ -1,9 +1,30 @@
 @extends('laravel-usp-theme::master')
 
 @section('content')
-to aqui: mudançã de status para: Negado ou Em processo de aquisição
+
+<form>
+<div class="form-group">
+    <label for="exampleFormControlSelect1">Mudança de status</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>Em processo de aquisição</option>
+      <option>Negado</option>
+
+    </select>
+  </div>
+
+</form>
 
 <br>
-Motivo caso seja negado
+
+<form>
+
+ <div class="form-group">
+    <label for="exampleFormControlTextarea1">Motivo caso for negado:</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+    <button type="button" class="btn btn-success">Enviar</button>
+  </div>
+
+</form>
 
 @endsection
