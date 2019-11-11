4935d07125177cd45c2ae37a34d4949e5c18aefb
Gabriela Dos Reis Silva <gabrielareisg@hotmail.com>
2019-11-11 18:35:56 -0300

    Banco de dados Areas de conhecimento

---
+++ database/migrations/2019_11_11_203355_create_areas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAreasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('areas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('codigo');//cod CAPES
+            $table->string('nome');//nome areas de conhecimento
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('areas');
+    }
+}
