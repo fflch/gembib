af8cfce21ec275679d17373b2e13fc6e7415522b
Gabriela Reis <gabrielareisg@hotmail.com>
2020-07-21 19:34:28 -0300

    etiquetas ok

--- app/Http/Controllers/EtiquetaController.php
+++ app/Http/Controllers/EtiquetaController.php
@@ -8,6 +8,7 @@ use Proner\PhpPimaco\Pimaco;
 use Proner\PhpPimaco\Tags\Barcode;
 use App\Item;
 use App\Utils\Util;
+use PDF;
 
 class EtiquetaController extends Controller
 {
@@ -45,28 +46,8 @@ class EtiquetaController extends Controller
             $barcode->setWidth(1);
 
             $limiteCaracteres = 10;
-
-            $tag->p("
-        <table style='width:100%; padding:1px; border: 0px solid #000'>
-        <tr>
-            <td style='width:60%;'>" .
-                "<span style='font-size: 9px'>
-                <b>Verba: </b>" . Util::limita_caracteres($item->verba , $limiteCaracteres) . "<br>" .
-                "<b>Aquisição: </b>" . Util::limita_caracteres($item->tipo_aquisicao , $limiteCaracteres) . "<br>" .
-                "<b>Processo: </b>" . $item->processo . "<br>" .
-                "<b>NF: </b>" . $item->nota_fiscal . "<br>" .
-                "<b>Preço: </b> R$ " . $item->preco . "<br>" .
-                "<b>Fornecedor: </b>" . Util::limita_caracteres($item->fornecedor , $limiteCaracteres) . "<br>" . 
-                "<b>Título: </b>" . Util::limita_caracteres($item->titulo , $limiteCaracteres) . "<br>" .
-                "<b>Autor: </b>" . Util::limita_caracteres($item->autor , $limiteCaracteres) . "<br>"   
-            ."</span></td>
-            <td style='text-align:center;'>"
-                . $item->tombo  
-                . $barcode->render() ."<br>SBD/FFLCH" .
-            "</td>
-        </tr>
-        </table>
-            ");
+            $codigo = $barcode->render();
+            $tag->p(view('pdfs.etiquetas', compact ('itens', 'codigo')));
             $pimaco->addTag($tag);
         }
 
--- resources/views/pdfs/etiquetas.blade.php
+++ resources/views/pdfs/etiquetas.blade.php
@@ -1,10 +1,10 @@
+@foreach($itens as $item)
 <table style='width:100%; padding:1px; border: 0px solid #000'>
     <tr>
         <td style='width:60%;'>
-        @foreach($itens as $item)
             <span style='font-size: 9px'>
-            <b>Verba: </b>{{ $item->verba, $limiteCaracteres }}<br>
-            <b>Aquisição: </b>{{ $item->tipo_aquisicao , $limiteCaracteres }}<br>
+            <b>Verba: </b>{{ $item->verba }}<br>
+            <b>Aquisição: </b>{{ $item->tipo_aquisicao }}<br>
             <b>Processo: </b> {{ $item->processo }}<br>
             <b>NF: </b> {{ $item->nota_fiscal }}<br>
             <b>Preço: </b> R${{ $item->preco }}<br>
@@ -12,12 +12,11 @@
             <b>Título: </b>{{ $item->titulo }}<br>
             <b>Autor: </b>{{ $item->autor }}<br>  
             </span>
-            @endforeach
         </td>
         <td style='text-align:center;'>
             {{ $item->tombo }}
-          
-            {{ $barcode->render() }} <br>SBD/FFLCH
+            {!! $codigo !!} SBD/FFLCH
         </td>
     </tr>
 </table>
+@endforeach
