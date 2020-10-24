b6753f009b2b46f1f809f9c44f3a410926bd4e8e
Gabriela Reis <gabrielareisg@hotmail.com>
2020-10-23 22:05:38 -0300

    tabela total

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -32,12 +32,40 @@
   <a href="/excel?status={{ request()->status }}&procedencia={{ request()->procedencia }}&busca={{ request()->busca }}">
   <i class="fas fa-file-excel"></i>Exportar busca em excel</a>  
 </div>
-    
 <br>
-
     {{ $itens->appends(request()->query())->links() }}
 <table class="table table-striped">
-  <p><b>Total de itens:</b> {{ $total }} </p>
+  <thead>
+  <tr align="center">
+    <th scope="col">Sugestão</th>
+    <th scope="col">Em Cotação:</th>
+    <th scope="col">Em Licitação:</th>
+    <th scope="col">Em Tombamento:</th>
+    <th scope="col">Negado:</th>
+    <th scope="col">Tombado:</th>
+    <th scope="col">Em Processamento Técnico:</th>
+    <th scope="col">Processado:</th>
+    <th scope="col">Total de itens:</th>
+    <th scope="col">Total por busca:</th>
+  </tr>
+  </thead>
+  <tbody>
+    <tr align="center">
+      <td>{{ $sugestao }}</td>
+      <td>{{ $cotacao }}</td>
+      <td>{{ $licitacao }}</td>
+      <td>{{ $tombamento }}</td>
+      <td>{{ $negado }}</td>
+      <td>{{ $tombado }}</td>
+      <td>{{ $processamento }}</td>
+      <td>{{ $processado }}</td>
+      <td>{{ $total }}</td>
+      <td>{{ $busca }}</td>
+    </tr>
+  </tbody>
+</table>
+
+<table class="table table-striped">
   <thead>
     <tr>
       <th scope="col">Tombo</th>
