030640a472ac77e009689dd268306ca6e94fc89d
Gabriela Reis <gabrielareisg@hotmail.com>
2021-04-20 21:22:53 -0300

    migration controle

---
+++ database/migrations/2021_04_20_204843_create_controles_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateControlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('controles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            // Campos a serem preenchidos pelo usuário
+            $table->date('data_processamento_inicio')->nullable();
+            $table->date('data_processamento_fim')->nullable();
+            $table->integer('titulos_novos')->nullable();
+            $table->integer('volumes')->nullable();
+            $table->integer('consistencia_acervo')->nullable();
+            $table->integer('outro_material')->nullable();
+            $table->integer('multimeios')->nullable();
+            $table->integer('servicos_tecnicos')->nullable();
+            $table->integer('remocoes_acervo')->nullable();
+            $table->text('observacao')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('controles');
+    }
+}
