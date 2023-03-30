ef97c86c450f04e2540c29e304f6e10eadd9b726
Brener Badures <brener_bg@hotmail.com>
2023-03-30 13:00:18 -0300

    Adicionado Javascript para impedir invios duplicados

--- resources/views/item/create.blade.php
+++ resources/views/item/create.blade.php
@@ -15,4 +15,31 @@
     <br>
 </form>
 
+@endsection('content')
+
+@section('javascripts_bottom')
+    <script>
+        jQuery.fn.preventDoubleSubmission = function() {
+        $(this).on('submit',function(e){
+            var $form = $(this);
+            if ($form.data('submitted') === true) {
+                e.preventDefault();
+            } else {
+                $form.data('submitted', true);
+            }
+        });
+            return this;
+        };
+
+        $('form').preventDoubleSubmission();
+        jQuery.fn.disableEnter = function() {
+            $('form').keypress(function(e) {
+            if (e.which == 13) {
+                return false;
+            }
+            });
+        };
+        $('form').disableEnter();
+
+    </script>
 @endsection
