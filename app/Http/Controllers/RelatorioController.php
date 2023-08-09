0b66d7420f0d48049cd4282238ab60c9b9a2e106
Brener Badures <brener_bg@hotmail.com>
2023-08-09 15:22:39 -0300

    Adicionar data ao pdf -Adicionada função de mostrar a data em conjunto à quantidade de páginas do PDF

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Item;
+use Carbon\Carbon;
 use PDF;
 use DB;
 
@@ -16,6 +17,7 @@ class RelatorioController extends Controller
 
     public function show(Request $request){
       $this->authorize('ambos');
+      $data_relatorio = Carbon::now()->format('d-m-Y');
       
       //na relatorio.pdf existem as variáveis titulo e titulo_relatorio. A titulo é utilizada para a pesquisa e a titulo_relatorio dará nome ao relatório criado
       $request->validate([
@@ -28,12 +30,12 @@ class RelatorioController extends Controller
       $soma = DB::table('itens')->select('preco')->where('cod_impressao', $request->cod_impressao)->get();
       $total = $soma->sum('preco');
 
-      $pdf = PDF::loadView('pdfs.relatorio', compact('itens','titulo_relatorio','total'));
+      $pdf = PDF::loadView('pdfs.relatorio', compact('itens','titulo_relatorio','total', 'data_relatorio'));
       $pdf->output();
       $dom_pdf = $pdf->getDomPDF();
 
       $canvas = $dom_pdf ->get_canvas();
-      $canvas->page_text(0, 0, "Page {PAGE_NUM} of {PAGE_COUNT}", null, 10, array(0, 0, 0));
+      $canvas->page_text(0, 0, "Página {PAGE_NUM} de {PAGE_COUNT} - Data: $data_relatorio", null, 10, array(0, 0, 0));
       return $pdf->download('relatorio.pdf');
     }
 }
