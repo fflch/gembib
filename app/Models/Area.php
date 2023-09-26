888bf66f87c2b0b243dd7322ec46f16f669c4057
Brener Badures <brener_bg@hotmail.com>
2023-09-26 16:28:29 -0300

    Alteração na criação de sugestões -Adicionados novos campos durante a criação de uma sugestão -Inclusão destes campos na view item

--- app/Models/Area.php
+++ app/Models/Area.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 
 class Area extends Model
 {
     use HasFactory;
+
+    const area = [
+        "Antropologia",
+        "Filosofia",
+        "Geografia",
+        "História",
+        "Letras Modernas",
+        "Letras Clássicas",
+        "Letras Orientais",
+        "Sociologia",
+        "Teoria Literária",
+        "Literatura Comparada",
+    ];
 }
