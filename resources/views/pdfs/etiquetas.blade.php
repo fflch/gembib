0bb5467e20caa27d2cbbc94d613376678d28ff97
Raphael <raphael.feitosa11@gmail.com>
2024-11-07 18:39:37 -0300

    Alterando o font-size para resolver o problema de espaço das etiquetas

--- resources/views/pdfs/etiquetas.blade.php
+++ resources/views/pdfs/etiquetas.blade.php
@@ -1,7 +1,7 @@
 <table style='width:100%; padding:2px; margin:{{$margem}}; border: 0px solid #000'>
     <tr>
         <td style='width:60%;'>
-            <span style='font-size: 9px'>
+            <span style='font-size: 8.5px'>
             @if(!empty($item->verba))<b>Verba: </b>{!! App\Utils\Util::limita_caracteres($item->verba , $limiteCaracteres) !!}<br>@endif
             <b>Aquisição: </b>{!! App\Utils\Util::limita_caracteres($item->tipo_aquisicao , $limiteCaracteres) !!}<br>
             @if(!empty($item->processo))<b>Processo: </b> {{ $item->processo }}<br>@endif
